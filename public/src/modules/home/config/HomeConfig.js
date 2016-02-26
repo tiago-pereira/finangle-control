f0d21d5aeb6df6e4d3ce9f79e4f383ec29e60f06
tiago-pereira <tiagops.prog@gmail.com>
2016-02-25 23:09:55 -0300

    Blocked home page without login

--- public/src/modules/home/config/HomeConfig.js
+++ public/src/modules/home/config/HomeConfig.js
@@ -2,16 +2,35 @@ define(function() {
 
   'use strict';
 
-  var HomeConfig = function($stateProvider, $urlRouterProvider) {
+  var HomeConfig = function($stateProvider, $urlRouterProvider){//}, AuthService, $q, $state, $timeout) {
     $stateProvider.state('home', {
       url: '/home',
       templateUrl: 'src/modules/home/views/index.html',
       controller: 'HomeCtrl',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      resolve: { authenticate: authenticate }
     });
+
+    function authenticate(AuthService, $q, $state, $timeout) {
+      if (AuthService.getUserStatus()) {
+        // Resolve the promise successfully
+        return $q.when()
+      } else {
+        // The next bit of code is asynchronously tricky.
+
+        $timeout(function() {
+          // This code runs after the authentication promise has been rejected.
+          // Go to the log-in page
+          $state.go('index')
+        })
+
+        // Reject the authentication promise to prevent the state from loading
+        return $q.reject()
+      }
+    }
   };
 
-  HomeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  HomeConfig.$inject = ['$stateProvider', '$urlRouterProvider',];//, 'AuthService', '$q', '$state', '$timeout'];
 
   return HomeConfig;
 });
