31cc82c41894f4b476b055f3152ca2bce147d09d
Tasey <tiagops.prog@gmail.com>
2016-02-29 20:42:51 -0300

    Created heroku variable for mongo

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url : 'mongodb://mongodb:123mongo321@apollo.modulusmongo.net:27017/mesAny8n'
+  url : process.env.MONGODB || 'mongodb://user:pass@apollo.modulusmongo.net:27017/mesAny8n'
 };
